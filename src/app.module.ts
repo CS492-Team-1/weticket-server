28ecfe291bfa967d1deaab8e593cae5b0fe49360
GwangjoGong <dwc05101@kaist.ac.kr>
2021-11-07 17:13:39 +0900

    Mongoose Module Configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,24 @@
+import * as Joi from 'joi';
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DB_CONNECTION_STRING: Joi.string().required(),
+      }),
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: 'src/schema.gql',
     }),
+    MongooseModule.forRoot(process.env.DB_CONNECTION_STRING),
   ],
   controllers: [AppController],
   providers: [AppService],
