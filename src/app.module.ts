992f4c7c2616ea9efde009da394935fb22c02f66
GwangjoGong <dwc05101@kaist.ac.kr>
2021-11-12 22:44:01 +0900

    좌석배치도 serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import * as Joi from 'joi';
+import { join } from 'path';
 
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -24,6 +26,9 @@ import { UserModule } from './user/user.module';
     MongooseModule.forRoot(process.env.DB_CONNECTION_STRING),
     JwtModule.forRoot({
       privateKey: process.env.JWT_PRIVATE_KEY,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static'),
     }),
     UserModule,
   ],
