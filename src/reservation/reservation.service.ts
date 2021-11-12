b73fc9932f52ab2ec43b18d5072e141911db7fb0
GwangjoGong <dwc05101@kaist.ac.kr>
2021-11-13 00:56:04 +0900

    Reservation service 구현

--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -1,4 +1,52 @@
+import { Model } from 'mongoose';
+
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { ReservationStatus } from './enums';
+import { Reservation, ReservationDocument } from './schemas';
 
 @Injectable()
-export class ReservationService {}
+export class ReservationService {
+  constructor(
+    @InjectModel(Reservation.name)
+    private reservations: Model<ReservationDocument>,
+  ) {}
+
+  async findById(reservationId: string) {
+    return this.reservations.findById(reservationId).exec();
+  }
+
+  async findByTimeAndSeat(time: Date, seat: number) {
+    return this.reservations
+      .findOne({
+        time,
+        seat,
+      })
+      .exec();
+  }
+
+  async create(time: Date, seat: number) {
+    const reservation = new this.reservations({
+      time,
+      seat,
+      status: ReservationStatus.PREEMPTED,
+    });
+
+    return reservation.save();
+  }
+
+  async findByTime(time: Date) {
+    return this.reservations.find({ time }).exec();
+  }
+
+  async updateStatus(reservationId: string, status: ReservationStatus) {
+    return this.reservations
+      .updateOne({ _id: reservationId }, { status })
+      .exec();
+  }
+
+  async delete(reservationId: string) {
+    return this.reservations.deleteOne({ _id: reservationId }).exec();
+  }
+}
